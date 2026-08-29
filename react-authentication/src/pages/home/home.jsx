fb0a04d362df757f760c5934bcf8d8d1fdb3f8e6
Azizullah <azizullahcodes@gmail.com>
2026-08-29 22:12:25 +0500

    added mdb bootstrap card in home.jsx page and map all products

--- react-authentication/src/pages/home/home.jsx
+++ react-authentication/src/pages/home/home.jsx
@@ -1,36 +1,140 @@
-import React, { useEffect, useState } from 'react'
+// import React, { useEffect, useState } from 'react';
+// import {
+//   MDBCard,
+//   MDBCardBody,
+//   MDBCardTitle,
+//   MDBCardText,
+//   MDBCardImage,
+//   MDBBtn,
+//   MDBRipple,
+//   MDBRow,
+//   MDBCol
+// } from 'mdb-react-ui-kit';
+
+// const Home = () => {
+//   const [fetchProducts,setFetchProducts] = useState([]);
+
+//   useEffect(()=>{
+//     let get = localStorage.getItem('productList');
+//     if(get){
+//       let jsonData = JSON.parse(get);
+//       // console.log(jsonData)
+//       jsonData && setFetchProducts(jsonData)
+//     }
+
+//   },[])
+//   console.log(fetchProducts)
+//   return (
+//     <div>
+//       <h1>Products screen</h1>
+    
+//         {
+//           (fetchProducts && fetchProducts.length >0)
+//           ?
+//   ( fetchProducts?.map((product,index)=>{
+//     return (
+//       <MDBRow>
+//         < MDBCol sm='6'>
+//       <MDBCard key={index}>
+//       <MDBRipple rippleColor='light' rippleTag='div' className='bg-image hover-overlay'>
+//         <MDBCardImage src={product.productImage} fluid alt={product.productName} 
+//         style={{
+//           height : 100,
+//           width : 80,
+//           objectFit : 'contain'
+//         }}/>
+//         <a>
+//           <div className='mask' style={{ backgroundColor: 'rgba(251, 251, 251, 0.15)' }}></div>
+//         </a>
+//       </MDBRipple>
+//       <MDBCardBody>
+//         <MDBCardTitle>
+//           {product.productName}
+//         </MDBCardTitle>
+//         <MDBCardText>
+//           {product.productDescription}
+//         </MDBCardText>
+//         <MDBBtn href='#'>Add to cart</MDBBtn>
+//       </MDBCardBody>
+//     </MDBCard>
+//     </MDBCol>
+//     </MDBRow>
+//     )
+//   })
+    
+//   )
+//           :
+//          (<h1>No product found</h1>)
+//         }
+      
+//     </div>
+//   )
+// }
+
+// export default Home
+
+import React, { useEffect, useState } from 'react';
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCardImage,
+  MDBBtn,
+  MDBRipple,
+  MDBRow,
+  MDBCol
+} from 'mdb-react-ui-kit';
 
 const Home = () => {
-  const [fetchProducts,setFetchProducts] = useState([]);
+  const [fetchProducts, setFetchProducts] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     let get = localStorage.getItem('productList');
-    if(get){
+    if (get) {
       let jsonData = JSON.parse(get);
-      // console.log(jsonData)
-      jsonData && setFetchProducts(jsonData)
+      jsonData && setFetchProducts(jsonData);
     }
+  }, []);
 
-  },[])
-  console.log(fetchProducts)
   return (
     <div>
       <h1>Products screen</h1>
-    
-        {
-          fetchProducts?.map((product,index)=>{
-            return  <li>
-    {/* <img src={product.productImage} alt={product.productName} /> */}
-    <img src={product.productImage} alt={product.productImage} />
-    <h3>{product.productName}</h3>
-    <p>{product.productDescription}</p>
-    <p>{product.productPrice}</p>
-  </li>
-          })
-        }
-      
-    </div>
-  )
-}
 
-export default Home
+      {(fetchProducts && fetchProducts.length > 0) ? (
+        <MDBRow>
+          {fetchProducts.map((product, index) => (
+            <MDBCol sm='4' key={index} className="mb-4">
+              <MDBCard>
+                <MDBRipple rippleColor='light' rippleTag='div' className='bg-image hover-overlay'>
+                  <MDBCardImage
+                    src={product.productImage}
+                    fluid
+                    alt={product.productName}
+                    style={{
+                      height: 100,
+                      width: 80,
+                      objectFit: 'contain'
+                    }}
+                  />
+                  <a>
+                    <div className='mask' style={{ backgroundColor: 'rgba(251, 251, 251, 0.15)' }}></div>
+                  </a>
+                </MDBRipple>
+                <MDBCardBody>
+                  <MDBCardTitle>{product.productName}</MDBCardTitle>
+                  <MDBCardText>{product.productDescription}</MDBCardText>
+                  <MDBBtn href='#'>Add to cart</MDBBtn>
+                </MDBCardBody>
+              </MDBCard>
+            </MDBCol>
+          ))}
+        </MDBRow>
+      ) : (
+        <h1>No product found</h1>
+      )}
+    </div>
+  );
+};
+
+export default Home;
